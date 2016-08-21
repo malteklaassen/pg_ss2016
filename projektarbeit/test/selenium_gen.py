11923ac17843dea76aacfb2c7b2a819824e02486
Malte Klaassen <klaassen@cs.uni-bonn.de>
2016-08-21 23:44:48 +0200

    Fixed python issues resulting from version differences

--- projektarbeit/test/selenium_gen.py
+++ projektarbeit/test/selenium_gen.py
@@ -7,14 +7,14 @@ url = 'http://localhost:8080'
 display = Display(visible=0, size=(800, 600))
 display.start()
 driver = webdriver.Chrome()
-print "Fetching /"
+print("Fetching /")
 driver.get(url + '/')
 sleep(2)
-print "Fetching /ssl.html"
+print("Fetching /ssl.html")
 driver.get(url + '/ssl.html')
 sleep(2)
-print "Fetching /malte/gm.html"
+print("Fetching /malte/gm.html")
 driver.get(url + '/malte/gm.html')
 sleep(2)
-print "Done"
+print("Done")
 
